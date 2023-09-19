f745d6c4e1854bb6743a07a9b0ce26cda70831c3
rashik07 <saif67090707@gmail.com>
2023-09-19 13:19:27 +0600

    vercel pb

--- routes/passportInfo.route.js
+++ routes/passportInfo.route.js
@@ -12,9 +12,6 @@ router.get("/get/all", passportInfoController.getAllPassportInfo);
 router.delete("/delete/:id", passportInfoController.deletePassportInfo);
 
 
-
-
-
 // router.post('/stats', upload.single('uploaded_file'), function (req, res) {
 //     // req.file is the name of your file in the form above, here 'uploaded_file'
 //     // req.body will hold the text fields, if there were any 
