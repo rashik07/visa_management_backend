56f60c8b2e05adf52bb9a0942eba33b462afaa39
rashik07 <saif67090707@gmail.com>
2023-10-11 00:49:57 +0600

    error

--- controllers/passportInfo.controller.js
+++ controllers/passportInfo.controller.js
@@ -1,44 +1,61 @@
 const PassportInfo = require("../modules/PassportInfo");
-const { getStorage, ref, getDownloadURL, uploadBytesResumable }=require("firebase/storage");
-const { dataPostService, getPassportInfo, getAllPassportInfo, deleteAllPassportInfo } = require("../services/passportInfo.service");
-
-
+const {
+  getStorage,
+  ref,
+  getDownloadURL,
+  uploadBytesResumable,
+} = require("firebase/storage");
+const {
+  dataPostService,
+  getPassportInfo,
+  getAllPassportInfo,
+  deleteAllPassportInfo,
+} = require("../services/passportInfo.service");
 
 const giveCurrentDateTime = () => {
   const today = new Date();
-  const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-  const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  const dateTime = date + ' ' + time;
+  const date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  const time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  const dateTime = date + " " + time;
   return dateTime;
-}
+};
 // const storage = getStorage();
 
 exports.postPassportInfo = async (req, res) => {
   const dateTime = giveCurrentDateTime();
   const storage = getStorage();
-  const storageRef = ref(storage, `files/${req.file.originalname + "       " + dateTime}`);
+  const storageRef = ref(
+    storage,
+    `files/${req.file.originalname + "       " + dateTime}`
+  );
 
   // Create file metadata including the content type
   const metadata = {
-      contentType: req.file.mimetype,
+    contentType: req.file.mimetype,
   };
 
   // Upload the file in the bucket storage
-  const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+  const snapshot = await uploadBytesResumable(
+    storageRef,
+    req.file.buffer,
+    metadata
+  );
   //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
 
   // Grab the public url
   const downloadURL = await getDownloadURL(snapshot.ref);
 
-  console.log('File successfully uploaded.');
-     const image=req.file.filename;
-     const name= req.body.name;
-     const passport= req.body.passport;
-   
+  console.log("File successfully uploaded.");
+  const image = req.file.filename;
+  const name = req.body.name;
+  const passport = req.body.passport;
+
   try {
     //   await wishlistItem.save();
     // res.status(200).json(req.file);
-    const data = await dataPostService(downloadURL,name,passport);
+    const data = await dataPostService(downloadURL, name, passport);
 
     res.status(200).send({ message: "Item added to wishlist" });
   } catch (error) {
@@ -69,8 +86,6 @@ exports.getAllPassportInfo = async (req, res) => {
   }
 };
 
-
-
 exports.updatePassportInfo = async (req, res) => {
   try {
     const { id } = req.params;
@@ -84,11 +99,33 @@ exports.updatePassportInfo = async (req, res) => {
         error: "No passport found. Please create an account",
       });
     }
+    const dateTime = giveCurrentDateTime();
+    const storage = getStorage();
+    const storageRef = ref(
+      storage,
+      `files/${req.file.originalname + "       " + dateTime}`
+    );
 
+    // Create file metadata including the content type
+    const metadata = {
+      contentType: req.file.mimetype,
+    };
+
+    // Upload the file in the bucket storage
+    const snapshot = await uploadBytesResumable(
+      storageRef,
+      req.file.buffer,
+      metadata
+    );
+    //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
+
+    // Grab the public url
+    const downloadURL = await getDownloadURL(snapshot.ref);
     // Update the passport information
-    if (req.file) {
-      passportInfoItem.image = req.file.filename;
+    if (downloadURL) {
+      passportInfoItem.image = downloadURL;
     }
+
     if (req.body.name) {
       passportInfoItem.name = req.body.name;
     }
@@ -120,9 +157,9 @@ exports.deletePassportInfo = async (req, res) => {
     if (!deletedItem) {
       return res.status(404).send({ error: "Item not found" });
     }
-//     if(res.status==200){
-// fs.unlink(path.join(__dirname,))
-//     }
+    //     if(res.status==200){
+    // fs.unlink(path.join(__dirname,))
+    //     }
     res.status(200).send({ message: "Item removed from wishlist" });
   } catch (error) {
     res.status(400).send({ error: error.message });
